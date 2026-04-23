81afa804795b6b4b3b4bdb69f5f4a5e6e34fda6e
Twilight <155774584+Davy7000@users.noreply.github.com>
2026-04-23 18:00:47 +0200

    Add initial set.js with configuration and functions

---
+++ set.js
@@ -0,0 +1,80 @@
+const a86_0x3c50ec = a86_0x5108;
+function a86_0x33a1() {
+  const _0xf3778e = ["BM9U", "sevst0Tvx0fqsv9lrvK", "mtmYotnlCxPrDha", "ndm0v1vHB21m", "mJeXmhreterxuG", "nde3wMrvCwPS", "DMvYC2LVBG", "l3nLDc5LBNy", "te9hx01trW", "AM9PBG", "t1DorvjFtLvnqKvs", "t1DorvjFtKfnrq", "Dg9tDhjPBMC", "ruXjrq", "BwfKzxDPDgGG8j+sMdTAt0Tpvs1nra", "y29UzMLN", "zg90zw52", "ndm4CNDLDvfS", "mta3nJe4nhPrEuPLqG", "thvMzNK", "zw52", "mZGWmNHryKHjta", "qvvut19et1Dote9brf9tvefuvvm", "mtaXnZzQvvjWs0y", "u1rjq0TfuL9qqunltKfnrq", "ue1Fq0Hbvejpva", "Cgf0Aa", "zxHPC3rZu3LUyW", "tuvovv9usevnrq", "lI9MCMfTzxDVCMSVy29UC3rHBNq", "qu5usv9dt01nqu5ex1nqqu0", "wfHQvui", "B3vP", "C2vHCMnO", "ndy2mdG3nMzyBgrwva", "u0vtu0LptL9jra", "otCWnxHgEfrLqW", "C2v0lMvUDG", "mtqYnZyZsuXdy1zg"];
+  a86_0x33a1 = function () {
+    return _0xf3778e;
+  };
+  return a86_0x33a1();
+}
+function a86_0x5108(_0x3000e3, _0x53a156) {
+  const _0x4708fe = a86_0x33a1();
+  return a86_0x5108 = function (_0x520cc2, _0x327242) {
+    _0x520cc2 = _0x520cc2 - 449;
+    let _0x33a14b = _0x4708fe[_0x520cc2];
+    if (a86_0x5108.OYJgwn === undefined) {
+      var _0x51080f = function (_0xa65f78) {
+        const _0x1a2dd0 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=";
+        let _0x302651 = "", _0x1c8d71 = "", _0xedf52d = _0x302651 + _0x51080f;
+        for (let _0x4a8cda = 0, _0xc5407f, _0x3760c0, _0x2966a4 = 0; _0x3760c0 = _0xa65f78.charAt(_0x2966a4++); ~_0x3760c0 && (_0xc5407f = _0x4a8cda % 4 ? _0xc5407f * 64 + _0x3760c0 : _0x3760c0, _0x4a8cda++ % 4) ? _0x302651 += _0xedf52d.charCodeAt(_0x2966a4 + 10) - 10 !== 0 ? String.fromCharCode(255 & _0xc5407f >> (-2 * _0x4a8cda & 6)) : _0x4a8cda : 0) {
+          _0x3760c0 = _0x1a2dd0.indexOf(_0x3760c0);
+        }
+        for (let _0x38e3e9 = 0, _0x52bbde = _0x302651.length; _0x38e3e9 < _0x52bbde; _0x38e3e9++) {
+          _0x1c8d71 += "%" + ("00" + _0x302651.charCodeAt(_0x38e3e9).toString(16)).slice(-2);
+        }
+        return decodeURIComponent(_0x1c8d71);
+      };
+      a86_0x5108.UlJTxv = _0x51080f, _0x3000e3 = arguments, a86_0x5108.OYJgwn = true;
+    }
+    const _0x598cb3 = _0x4708fe[0], _0x31e7af = _0x520cc2 + _0x598cb3, _0xea31ef = _0x3000e3[_0x31e7af];
+    if (!_0xea31ef) {
+      const _0x504eff = function (_0x2d7295) {
+        this.MgaFfc = _0x2d7295, this.KBoQWs = [1, 0, 0], this.iRnINw = function () {
+          return "newState";
+        }, this.vBiMPE = "\\w+ *\\(\\) *{\\w+ *", this.QSrnvY = "['|\"].+['|\"];? *}";
+      };
+      _0x504eff.prototype.zqVFSU = function () {
+        const _0x2c6b05 = new RegExp(this.vBiMPE + this.QSrnvY), _0x1ad367 = _0x2c6b05.test(this.iRnINw.toString()) ? --this.KBoQWs[1] : --this.KBoQWs[0];
+        return this.UURjTx(_0x1ad367);
+      }, _0x504eff.prototype.UURjTx = function (_0x275f0c) {
+        if (!Boolean(~_0x275f0c)) return _0x275f0c;
+        return this.DTengz(this.MgaFfc);
+      }, _0x504eff.prototype.DTengz = function (_0x22a490) {
+        for (let _0x4e6a3e = 0, _0x2e6612 = this.KBoQWs.length; _0x4e6a3e < _0x2e6612; _0x4e6a3e++) {
+          this.KBoQWs.push(Math.round(Math.random())), _0x2e6612 = this.KBoQWs.length;
+        }
+        return _0x22a490(this.KBoQWs[0]);
+      }, new _0x504eff(a86_0x5108).zqVFSU(), _0x33a14b = a86_0x5108.UlJTxv(_0x33a14b), _0x3000e3[_0x31e7af] = _0x33a14b;
+    } else _0x33a14b = _0xea31ef;
+    return _0x33a14b;
+  }, a86_0x5108(_0x3000e3, _0x53a156);
+}
+(function (_0x1131c7, _0x1ce8ba) {
+  const _0x996a66 = a86_0x5108, _0x599a30 = _0x1131c7();
+  while (true) {
+    try {
+      const _0x4b33b1 = parseInt(_0x996a66(462)) / 1 + -parseInt(_0x996a66(484)) / 2 * (-parseInt(_0x996a66(468)) / 3) + parseInt(_0x996a66(481)) / 4 + -parseInt(_0x996a66(460)) / 5 * (-parseInt(_0x996a66(480)) / 6) + parseInt(_0x996a66(466)) / 7 * (parseInt(_0x996a66(486)) / 8) + parseInt(_0x996a66(465)) / 9 * (-parseInt(_0x996a66(467)) / 10) + -parseInt(_0x996a66(458)) / 11;
+      if (_0x4b33b1 === _0x1ce8ba) break; else _0x599a30.push(_0x599a30.shift());
+    } catch (_0x4813dc) {
+      _0x599a30.push(_0x599a30.shift());
+    }
+  }
+}(a86_0x33a1, 161242));
+const a86_0x327242 = function () {
+  let _0x2ec73a = true;
+  return function (_0x4a16be, _0x2e8e6e) {
+    const _0x2a3170 = _0x2ec73a ? function () {
+      if (_0x2e8e6e) {
+        const _0xf866e6 = _0x2e8e6e.apply(_0x4a16be, arguments);
+        return _0x2e8e6e = null, _0xf866e6;
+      }
+    } : function () {};
+    return _0x2ec73a = false, _0x2a3170;
+  };
+}(), a86_0x520cc2 = a86_0x327242(this, function () {
+  const _0x435219 = a86_0x5108, _0x4adf74 = {XXjUB: "(((.+)+)+)+$"};
+  return a86_0x520cc2.toString()[_0x435219(457)](_0x4adf74[_0x435219(455)])[_0x435219(475)]().constructor(a86_0x520cc2)[_0x435219(457)]("(((.+)+)+)+$");
+});
+a86_0x520cc2();
+const fs = require("fs-extra"), path = require(a86_0x3c50ec(450)), {TEMP_DIR} = require(a86_0x3c50ec(453));
+if (fs[a86_0x3c50ec(451)](a86_0x3c50ec(461))) require(a86_0x3c50ec(479))[a86_0x3c50ec(478)]({path: __dirname + a86_0x3c50ec(470)});
+process.env.TEMP = TEMP_DIR, module.exports = {VERSION: require("./package.json")[a86_0x3c50ec(469)], DATABASE_PATH: path[a86_0x3c50ec(472)](__dirname, "database.db"), STORE_PATH: path[a86_0x3c50ec(472)](__dirname, "store.db"), SESSION_ID: process.env[a86_0x3c50ec(459)] || null, PRESENCE: process[a86_0x3c50ec(483)].PRESENCE, PREFIX: process[a86_0x3c50ec(483)].PREFIX || ".", OWNER_NAME: process[a86_0x3c50ec(483)][a86_0x3c50ec(474)] || a86_0x3c50ec(482), OWNER_NUMBER: process[a86_0x3c50ec(483)][a86_0x3c50ec(473)] || null, AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || a86_0x3c50ec(463), AUTO_DOWNLOAD_STATUS: process[a86_0x3c50ec(483)][a86_0x3c50ec(485)] || "non", MODE_PUBLIC: process[a86_0x3c50ec(483)].MODE_PUBLIC || a86_0x3c50ec(456), PM_PERMIT: process.env.PM_PERMIT || a86_0x3c50ec(463), BOT_NAME: process.env.BOT_NAME || "Zokou_MD", MENU_THEME: process[a86_0x3c50ec(483)][a86_0x3c50ec(452)] || a86_0x3c50ec(476), HEROKU_APP_NAME: process[a86_0x3c50ec(483)].HEROKU_APP_NAME || null, HEROKU_API_KEY: process.env[a86_0x3c50ec(464)] || null, WARN_COUNT: process[a86_0x3c50ec(483)].WARN_COUNT || "3", STARTING_BOT_MESSAGE: process.env.STARTING_BOT_MESSAGE || "oui", PM_CHATBOT: process.env[a86_0x3c50ec(449)] || "non", SERVER: process[a86_0x3c50ec(483)].SERVER || null, ANTI_COMMAND_SPAM: process[a86_0x3c50ec(483)][a86_0x3c50ec(454)] || "non", ANTI_DELETE_MESSAGE: process.env.ANTI_DELETE_MESSAGE || "oui", AUTO_REACT_MESSAGE: process[a86_0x3c50ec(483)].AUTO_REACT_MESSAGE || a86_0x3c50ec(463), AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || a86_0x3c50ec(463), TIME_ZONE: process[a86_0x3c50ec(483)].TIME_ZONE || "Africa/Sao_Tome", STICKER_PACKNAME: process.env[a86_0x3c50ec(487)] || a86_0x3c50ec(477), DATABASE_URL: process[a86_0x3c50ec(483)].DATABASE_URL || null, LOG_MSG: process[a86_0x3c50ec(483)][a86_0x3c50ec(471)] || "non"};
