1e4db4f3d8f082f730b09708385dbc45cb172d3b
Twilight <155774584+Davy7000@users.noreply.github.com>
2026-06-05 00:54:36 +0200

    Change command pattern to 'teddy' and update response

--- plugins/hello.js
+++ plugins/hello.js
@@ -3,10 +3,40 @@ const config = require('../config');
 const isPrivateBot = config.MODE !== 'public';
 
 Module({
-    pattern: 'hello ?(.*)',
+    pattern: 'teddy ?(.*)',
     fromMe: isPrivateBot,
-    desc: 'Says hello',
-    type: 'misc'
+    desc: "Envoie un ours animé avec un bombardement d'amour signé Synapse X",
+    type: 'fun'
 }, async (message, match) => {
-    await message.sendReply(`Salut Boss je suis Synapse-X prêt pour vous servire`);
+    // Liste enrichie d'emojis pour l'animation
+    let emo = [
+        '❤️', '🧡', '💛', '💚', '💙', '💜', '🖤', '🤍', '🤎', '💖', 
+        '💝', '✨', '🔥', '🥰', '😍', '🤩', '😘', '💋', '🌹', 
+        '🌷', '🌸', '💐', '🎈', '🎉', '🧸', '❤️', '💘'
+    ];
+
+    // Message d'envoi initial
+    const sent_msg = await message.sendReply("*_Je t'aime trés fort_*");
+
+    // Boucle d'animation
+    for (let i = 0; i < emo.length; i++) {
+        const emoji = emo[i];
+        
+        // Délai d'attente de 800ms
+        await new Promise(res => setTimeout(res, 800));
+
+        let teddy = `*(\\_/)*\n*( •.•)*\n*/>* 🤍`; 
+        
+        let messageAnime = teddy.replace("🤍", emoji);
+
+        // Touche personnelle : Signature finale ancrée grâce au symbole ">"
+        if (i === emo.length - 1) {
+            messageAnime += `\n\n` +
+                
+               `> _Design original par l'OSS Company_\n`;
+        }
+
+        // Modification en temps réel
+        await message.edit(messageAnime, message.jid, sent_msg.key);
+    }
 });
