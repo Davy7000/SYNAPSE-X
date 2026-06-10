ea6c250920e0f2adc8e84b3a72a4e1003b8f72e1
Twilight <155774584+Davy7000@users.noreply.github.com>
2026-06-10 03:57:01 +0200

    Simplify restart module by removing reload and reboot
    Removed redundant reload and reboot commands, keeping only the restart functionality.

--- plugins/restart.js
+++ plugins/restart.js
@@ -2,30 +2,6 @@ const { Module } = require("../main");
 
 Module(
   {
-    pattern: "reload",
-    fromMe: true,
-    excludeFromCommands: true,
-  },
-  async (m) => {
-    await m.sendReply("_Reloading bot..._");
-    process.exit(0);
-  }
-);
-
-Module(
-  {
-    pattern: "reboot",
-    fromMe: true,
-    excludeFromCommands: true,
-  },
-  async (m) => {
-    await m.sendReply("_Reloading bot..._");
-    process.exit(0);
-  }
-);
-
-Module(
-  {
     pattern: "restart",
     fromMe: true,
     desc: "Restarts the bot",
