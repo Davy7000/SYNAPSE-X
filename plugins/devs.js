67f48f44ad008647a731f6bee3c838a0ecccc832
Twilight <155774584+Davy7000@users.noreply.github.com>
2026-06-14 00:07:16 +0100

    Add files via upload

---
+++ plugins/devs.js
@@ -0,0 +1,36 @@
+const { Module } = require('../main');
+const config = require('../config');
+
+const isPrivateBot = config.MODE !== 'public';
+
+Module({
+    pattern: 'devs',
+    fromMe: isPrivateBot,
+    desc: 'Présenter les développeurs officiels du projet Synapse-X',
+    use: 'oss'
+}, async (message, match) => {
+    try {
+        let ownerMessage = `👑 *[ FONDATEURS DE SYNAPSE-X ]*\n`;
+        ownerMessage += `━━━━━━━━━━━━━━━━━━━━━━\n\n`;
+        
+        ownerMessage += `🧠 *T W I L I G H T³₆⁹ 모* (Génie Concepteur)\n`;
+        ownerMessage += `• *Rôle :* Développeur Principal & Architecte Core\n`;
+        ownerMessage += `• *Spécialité :* Logique système, optimisation VPS & infrastructure\n\n`;
+        
+        ownerMessage += `✨ *K A R M A ³₆⁹ 모* (Génie Concepteur)\n`;
+        ownerMessage += `• *Rôle :* Concepteur UI/UX & Intégrateur de Modules\n`;
+        ownerMessage += `• *Spécialité :* Gestion des flux, Design & interactivité\n\n`;
+        
+        ownerMessage += `🏢 *Organisation :* OSS Company\n\n`;
+        ownerMessage += `🇨🇬 *Origine :* République du Congo\n`;
+        ownerMessage += `━━━━━━━━━━━━━━━━━━━━━━\n`;
+        ownerMessage += `> 💡 _L'innovation n'est rien sans une exécution parfaite._\n`;
+        ownerMessage += `> *Propulsé par l'OSS Company* 🌐 https://ossynapse.netlify.app/oss_company\n`;
+
+        await message.sendReply(ownerMessage);
+
+    } catch (error) {
+        console.error('Erreur dans le plugin owner :', error);
+        await message.sendReply('❌ Une erreur est survenue lors de l\'affichage des informations des développeurs.');
+    }
+});
